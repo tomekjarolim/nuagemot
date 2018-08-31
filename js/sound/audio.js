4d50dfd3890d4f742c2c390bfa036f9124d4de8e
Denez Thomas <denezthomas@gmail.com>
2018-08-31 13:41:00 +0200

    Fix player and recorder

--- js/sound/audio.js
+++ js/sound/audio.js
@@ -11,9 +11,7 @@ let rate = 1;
 let micBuffer;
 let master = new p5.Gain();
 let granulationGain = new p5.Gain();
-// TEMP FIX /////////////////////////
-let granulationGain2 = new p5.Gain();
-/////////////////////////////////////
+let micGranulationGain = new p5.Gain();
 let micGain = 30; // You may need to increase it if your microphone isn't sensitive enough
 window.y; // global variable for yin (wavesjs-lfo)
 let freq;
@@ -35,8 +33,6 @@ let isSchedulerOn = false;
 // Called from preload() in main.js
 function preloadSounds() {
     whispers = loadSound('js/assets/whispers.mp3');
-    // TEMP
-    micSound = loadSound('js/assets/mic.mp3');
 }
 
 // Called from setup() in main.js
@@ -51,27 +47,23 @@ function audioSetup() {
 		// analyzer
 		fft = new p5.FFT();
 		audioAnalyser();
-		// Player
-		whispers.disconnect();
-		// TEMP FIX
-		micSound.disconnect();
-		//granulationGain = new p5.Gain();
-		granulationGain.setInput(whispers);
-		granulationGain.connect(master);
-		//TEMP FIX
-		granulationGain2.setInput(micSound);
-		granulationGain2.connect(master);
-		playerrr();
         // Recorder
         recorder = new p5.SoundRecorder();
         recorder.setInput(mic);
         micBuffer = new p5.SoundFile();
+        // Player
+		whispers.disconnect();
+		micBuffer.disconnect();
+		granulationGain.setInput(whispers);
+		granulationGain.connect(master);
+		micGranulationGain.setInput(micBuffer);
+		micGranulationGain.connect(master);
+		playerrr();
 	}
 }
 
 // Called from draw() in main.js
 function audioLoop() {
-	//if (micOn && !isRecording) recordr(); 
 }
 
 //	Random granulation player, amplitude is controlled by mic input
@@ -83,14 +75,12 @@ function playerrr() {
 		if (freq >= 70 && freq <= 580) rate = map(freq, 70, 580, 0.8, 1.7);
 		whispers.play(0, rate, 1, offset, duration); //Man rate 0.85
 	} else {
-		// if (micBuffer) {
-		// 	let offset = random(0, (micBuffer.duration - duration));
-		// 	micBuffer.play(0, rate, 1, offset, duration);
-		// }
-		//TEMP FIX
-		let offset = random(0, (micSound.duration() - duration));
-		micSound.play(0, 1, 1, offset, duration);
+		if (micBuffer && eloquence >= 75){
+			let offset = random(0, (micBuffer.duration() - duration));
+			micBuffer.play(0, rate, 1, offset, duration);
+		}
 	}
+
     let metroPlayer = setTimeout(playerrr, density);
 }
 
@@ -99,7 +89,7 @@ function audioAnalyser() {
 	if (micOn){
 		amp = mic.getLevel();
 		granulationGain.amp(amp * micGain, 0.1, 0);
-		granulationGain2.amp(amp * micGain, 0.1, 0);
+		micGranulationGain.amp(amp * micGain, 0.1, 0);
 		fft.analyze();
 		centroid = fft.getCentroid();
 		if (window.y && window.y.pitch !== -1) freq = window.y.pitch;
@@ -109,37 +99,44 @@ function audioAnalyser() {
 			isSchedulerOn = true;
 			scheduler();
 			console.log("Scheduler started!");
+
 		}
 
 		let analyserRefresh = setTimeout(audioAnalyser, audioPollFreq);
 	}
 }
 
+// Dynamic timeline handler
 function scheduler() {
+	let timer;
 	if(isSchedulerOn === true) {
-		//console.log("Scheduler triggered");
-		//console.log('isSchedulerOn?' + isSchedulerOn);
 		let zeroTimer;
 		if (isTalking){
 			speechDur++;
-			eloquence += 4; // ++ faster than --
+			eloquence += 2; // ++ faster than --
 		} else {
 			silenceDur++;
 			eloquence--; 
 		}
 		eloquence = constrain(eloquence, 0, 100);
 		console.log(eloquence);
-		// Is this the end ?
-		if (micOn && eloquence === 0){
-			//console.log("timeouts triggered"); 
-			// Word is displayed n seconds after eloquence hit zero
+		// Start recording as soon as possible
+		if (!hasRecorded && isTalking && eloquence <= 75) {
+			startRecorder();
+		}
+		// Stop recording when user has talked enough
+		if (eloquence >= 75){
+			stopRecorder();
+		}
+		// When eloquence is at its minimum, 
+		if (micOn && eloquence === 0){ 
+			// Word is displayed when eloquence hit zero
 			console.log("Eloquence gauge at zero, shutting down audio and displaying the word!"); 
+			//Stop recorder if user didn't talk enough, and turn mic off (and whispers)
+			stopRecorder();
 			micOn = false;
-			// Stop recorder if user didn't talk enough
-			// if (isRecording) {
-			// 	recorder.stop(); 
-			// 	console.log('Recording stopped!');
-			// }
+			granulationGain.amp(0, 1, 0);
+			micGranulationGain.amp(0, 1, 0);
 			// End is triggered n seconds after word is displayed
 			theEnd = setTimeout(() => {
 				console.log("End of loop, listening for the next user...");
@@ -152,46 +149,39 @@ function scheduler() {
 				silenceDur = 0; 
 				eloquence = 50;
 				hasRecorded = false;
-				//recordr(); 
 			}, afterWordDuration);
 		}		
 	}
 	
-	let timer = setTimeout(scheduler, timerInterval);
+	timer = setTimeout(scheduler, timerInterval);
 }
 
-
+// Choose whether it's one or the other buffer that will be played
+// by the playerrr() function according to eloquence gauge
 function chooseBuffer() {
 	let bufferChoiceProbability = 1 - (eloquence / 100) + 0.7;
 	let prob = constrain(bufferChoiceProbability, 0.7, 1.0);
-	//console.log(prob);
 	let bufferChoice = random();
 	if (bufferChoice <= prob) return 0;
 	else return 1;
 } 
 
-
-
-// Recorder : isRecording is not triggered! See audioLoop() (test prevents recodr() to be called)
-function recordr() {
-	// if (!hasRecorded && isTalking && eloquence <= 75) {
- //       	recorder.record(micBuffer);
- //       	hasRecorded = true;
- //       	isRecording = true; 
- //       	console.log('Recording audio...');
- //       	console.log('Eloquence :' + eloquence);
-	// } 
-	// if (eloquence >= 75 && hasRecorded === true) {
-	// 	console.log('Recording stopped!');
-	// 	recorder.stop();
-	// 	isRecording = false;
-	// 	console.log('Recording stopped!');
-	// }
+// Start recording microphone into a buffer for later use
+function startRecorder() {
+	if (!isRecording) {
+	    recorder.record(micBuffer);
+	    isRecording = true; 
+	    console.log('Recording audio...');
+	    console.log('Eloquence :' + eloquence);
+	}
 }
 
-
-
-
-
-
-
+// Stop recording to be able to use the buffer with the playerrr() function
+function stopRecorder() {
+	if (isRecording) {		
+		recorder.stop();
+		isRecording = false;
+		hasRecorded = true;
+		console.log('Recording stopped!');
+	}
+}
